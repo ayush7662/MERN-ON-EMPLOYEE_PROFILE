2f34fb82fee7f836f2e86c67124ac78a956e04e0
AYUSH RAJ <127328301+ayush7662@users.noreply.github.com>
2024-06-29 17:22:42 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,18 @@
+const youtuber ="thapa technical";
+
+const favprog ="React js";
+
+function myname(){
+  let name ="vinod";
+  return name;
+}
+
+function myNames(){
+  let names ="vinod thapa";
+  return names;
+}
+
+export default youtuber;
+
+export {favprog, myname, myNames };
+
